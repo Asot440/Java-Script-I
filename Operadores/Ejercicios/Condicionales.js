ff107d455272df3b42a00e61fe4932569d8da39a
Adrian Martinez <asot_buuren440@hotmail.com>
2025-11-16 09:21:22 -0600

    Se agrega carpeta operadores y se comienza ejercicio de control de flujo

---
+++ Operadores/Ejercicios/Condicionales.js
@@ -0,0 +1,8 @@
+var numero1 = parseInt(prompt("Introduce el primer número:",0));
+var numero2 = parseInt(prompt("Introduce el segundo número:",0));
+
+console.log(numero1, numero2)
+
+
+alerta=numero1==numero2?"Los numeros son iguales":"Los numeros son diferentes"
+alert(alerta);
