401b47d1a9369413ba7e5135c3db382eade5853e
Adrian Martinez <asot_buuren440@hotmail.com>
2025-11-18 14:00:30 -0600

    Se agrega ejercicio 1 de la actividad guiada

--- Operadores/Ejercicios/Condicionales.js
+++ Operadores/Ejercicios/Condicionales.js
@@ -1,8 +1,23 @@
 var numero1 = parseInt(prompt("Introduce el primer número:",0));
 var numero2 = parseInt(prompt("Introduce el segundo número:",0));
 
-console.log(numero1, numero2)
+console.log(`Numero uno: ${numero1}, Numero dos: ${numero2}`)
 
+if(numero1===numero2){
+    alert("Los numeros son iguales");
+}
+else if(numero1>numero2){
+    alert(`El número mayor es ${numero1}`);
+    alert(`El número menor es ${numero2}`);
+}
+else if(numero2>numero1){
+    alert(`El numero mayor es: ${numero2}`);
+    alert(`El numero menor es: ${numero1}`);
+}
+else{
+    alert("Introduce numeros correctos");
+}
 
-alerta=numero1==numero2?"Los numeros son iguales":"Los numeros son diferentes"
-alert(alerta);
+/*alerta=numero1==numero2?"Los numeros son iguales":"Los numeros son diferentes"
+alert(alerta);*/
+
